defeb111999c796735e21b279a9608c8c904f809
Tomáš Čech <sleep_walker@gnu.org>
2020-01-19 10:49:01 +0100

    main: clear text input on reset

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Root(FloatLayout):
 
     def reset(self):
         self.dt.reset()
+        self.textbox.text = ""
         self.generate_question()
         Clock.schedule_once(self.set_focus_to_textinput, 0.1)
 
